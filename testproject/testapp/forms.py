9583b2ac5b8055711215a5496bb14bdab147ee5e
Marco Braak <mbraak@ridethepony.nl>
2012-05-14 16:08:32 +0200

    Fixed import in test forms

--- testproject/testapp/forms.py
+++ testproject/testapp/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from django_pony_forms.simple_form import SimpleFormMixin
+from django_pony_forms.pony_forms import PonyFormMixin
 
 
-class ExampleForm(SimpleFormMixin, forms.Form):
+class ExampleForm(PonyFormMixin, forms.Form):
     name = forms.CharField(max_length=50, required=True, help_text='help text')
     description = forms.CharField(max_length=255, help_text='please fill in a description', widget=forms.Textarea, required=False)
     code = forms.CharField(max_length=15, required=True, widget=forms.HiddenInput)
