a56976ea581a2a3503e67920077f62c3ae1346f7
Marco Braak <mbraak@ridethepony.nl>
2015-01-21 11:31:23 +0100

    Removed django.contrib.sites from test project

--- testproject/settings.py
+++ testproject/settings.py
@@ -93,7 +93,6 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
